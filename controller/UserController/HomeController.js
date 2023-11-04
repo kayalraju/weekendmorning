ba4ff30824df7354b49ce05afaf972cc2ec054dd
kayalraju <kayalraju003@gmail.com>
2023-11-04 08:42:57 +0530

    dd

--- controller/UserController/HomeController.js
+++ controller/UserController/HomeController.js
@@ -1,13 +1,49 @@
+const User=require('../../model/userModel')
 
-
-
+const fetchdata=async(req,res)=>{
+    try{
+        const allData=await User.find()
+        res.render('User/fetchdata',{
+            data:allData
+        })
+        console.log('jjj',allData);
+    }catch(err){
+        console.log(err);
+    }
+   
+}
 
 const HomePage=(req,res)=>{
     res.render('User/Home')
 
 }
+const creatreUser=async(req,res)=>{
+
+    try{
+        const Udata=await new User({
+            name:req.body.name,
+            email:req.body.email,
+            city:req.body.city,
+        })
+        const data=await Udata.save()
+        console.log(data);
+        if(data){
+            res.redirect('/fetchdata')
+        }else{
+            res.redirect('/')
+        }
+
+    }catch(error){
+        console.log(error);
+    }
+   
+
+}
+
 
 
 module.exports={
-    HomePage
+    HomePage,
+    creatreUser,
+    fetchdata
 }
--- router/userRoute/HomeRoute.js
+++ router/userRoute/HomeRoute.js
@@ -1,8 +1,10 @@
 const express=require('express');
-const { HomePage } = require('../../controller/UserController/HomeController');
+const { HomePage, creatreUser, fetchdata } = require('../../controller/UserController/HomeController');
 const Route=express.Router();
 
 Route.get('/',HomePage)
+Route.post('/user/create',creatreUser)
+Route.get('/fetchdata',fetchdata)
 
 
 
